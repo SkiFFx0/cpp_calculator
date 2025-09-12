5fbdab13ba63939b9c0f4bd9763508c917704b63
SkiFF <dserik228@gmail.com>
2025-09-12 22:33:31 +0500

    feat: add 2 possible input variables and 4 possible basic operations

--- main.cpp
+++ main.cpp
@@ -3,11 +3,30 @@ using namespace std;
 
 int main() {
     cout << "Welcome to the calculator.\n";
-    cout << "Enter a number: ";
 
-    double num;
-    cin >> num;
+    double a, b;
+    char op;
 
-    cout << "You entered: " << num << "\n";
+    cout << "Enter first number: ";
+    cin >> a;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter second number: ";
+    cin >> b;
+
+    double result;
+
+    if (op == '+') result = a + b;
+    else if (op == '-') result = a - b;
+    else if (op == '*') result = a * b;
+    else if (op == '/') result = a / b;
+    else {
+        cout << "Invalid operator";
+        return 1;
+    }
+
+    cout << "Result: " << result;
     return 0;
 }
