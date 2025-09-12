f25e40fc761cce6e1452e0c1081d20867f83439b
SkiFF <dserik228@gmail.com>
2025-09-12 22:19:31 +0500

    feat: add input and output of a variable

--- main.cpp
+++ main.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main() {
     cout << "Welcome to the calculator.\n";
+    cout << "Enter a number: ";
+
+    double num;
+    cin >> num;
+
+    cout << "You entered: " << num << "\n";
     return 0;
 }
